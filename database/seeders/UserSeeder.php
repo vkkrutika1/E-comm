ae92cade67fb2685dc12f858949de7af627284e3
VkKrutika <91652800+VkKrutika@users.noreply.github.com>
2024-03-03 16:19:20 +0530

    login done

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('users')->insert([
+            'name'=>'Peter Parker',
+            'email'=>'peterparker@gmail.com',
+            'password'=>Hash::make('12345')
+        ]);
+    }
+}
